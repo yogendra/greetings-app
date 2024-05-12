b585013876e1bd82502d45badf558379a2a4fb37
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2024-05-12 15:30:21 +0800

    Adding intelligent default

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        dbname=os.environ.get("POSTGRES_DB"),
-        user=os.environ.get("POSTGRES_USER"),
+        dbname=os.environ.get("POSTGRES_DB", "postgres"),
+        user=os.environ.get("POSTGRES_USER", "postgres"),
         password=os.environ.get("POSTGRES_PASSWORD"),
-        host=os.environ.get("POSTGRES_HOST"),
+        host=os.environ.get("POSTGRES_HOST", "localhost"),
         port=os.environ.get("POSTGRES_PORT", 5432)
     )
     return conn
