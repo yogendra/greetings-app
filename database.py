f2409b7e7a2d2b86ee53f9673682efd1876023af
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2024-05-12 12:55:38 +0000

    use default password

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        dbname=os.environ.get("POSTGRES_DB", "postgres"),
+        dbname=os.environ.get("PGDATABASE", "postgres"),
         user=os.environ.get("POSTGRES_USER", "postgres"),
-        password=os.environ.get("POSTGRES_PASSWORD"),
+        password=os.environ.get("POSTGRES_PASSWORD", "postgres"),
         host=os.environ.get("POSTGRES_HOST", "localhost"),
         port=os.environ.get("POSTGRES_PORT", 5432)
     )
